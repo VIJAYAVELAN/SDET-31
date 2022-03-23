a3b39bfeb5e8d0eee5a25bd27035359da0e99d90
USER <USER@DESKTOP-3E9CJ1E>
2022-03-23 17:54:40 +0530

    Second Fetch

---
+++ crm.MavenDemo/src/test/java/crm/MavenDemo/deleteContactTest.java
@@ -0,0 +1,12 @@
+package crm.MavenDemo;
+
+import org.testng.annotations.Test;
+
+public class deleteContactTest {
+
+	@Test
+	
+	public void delContact() {
+		System.out.println("DeleteContact");
+	}
+}
